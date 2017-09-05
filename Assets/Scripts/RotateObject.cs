53fa32bf76ae8e826c4fcb495f2b91c823657fbe
Mythique <sv@44screens.com>
2017-09-05 14:52:00 -0400

    Added RotateObject script and main scene

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RotateObject : MonoBehaviour, IDragHandler
+{
+    public float _speed;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (Input.touchSupported)
+        {
+            transform.Rotate(new Vector3(0, -Input.touches[0].deltaPosition.x, 0) * Time.deltaTime * _speed * 0.5f);
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, -Input.GetAxis("Mouse X"), 0) * Time.deltaTime * _speed);
+        }
+    }
+}
