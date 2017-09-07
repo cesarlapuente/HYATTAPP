11c0e02fded3d1ce7a919e4296b3327c3fcbe768
Mythique <sv@44screens.com>
2017-09-07 12:37:36 -0400

    Updated UI
    - Added resources for the UI
    - Added three screens : Map View, Espace View, 3D view

---
+++ Assets/Scripts/UI/SpriteChanger.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Use this to easily switch between sprites if an Image component is attached to this object
+/// </summary>
+public class SpriteChanger : MonoBehaviour
+{
+    private Image _imageComponent;
+    public Sprite[] _sprites;
+    public bool _spriteEnabled = true;
+
+    private void Awake()
+    {
+        _imageComponent = transform.GetComponent<Image>();
+        if (_imageComponent == null)
+        {
+            Debug.LogError("There is no image component attached to this GameObject");
+        }
+        if (!_spriteEnabled)
+        {
+            DisableSprite();
+        }
+    }
+
+    public void ChangeSprite(uint spriteIndex)
+    {
+        if (!_spriteEnabled)
+        {
+            _spriteEnabled = true;
+            _imageComponent.color = new Color(_imageComponent.color.r, _imageComponent.color.g, _imageComponent.color.b, 1.0f);
+        }
+
+        if (spriteIndex < _sprites.Length)
+        {
+            _imageComponent.sprite = _sprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogWarning("Incorrect spriteIndex, the sprite was not changed");
+        }
+    }
+
+    public void DisableSprite()
+    {
+        _spriteEnabled = false;
+        _imageComponent.sprite = null;
+        _imageComponent.color = new Color(_imageComponent.color.r, _imageComponent.color.g, _imageComponent.color.b, 0.0f);
+    }
+}
