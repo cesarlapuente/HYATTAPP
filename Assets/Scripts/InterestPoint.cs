33654c533f68342c8c572a3d531a55635115914b
Mythique <sv@44screens.com>
2017-09-18 11:47:30 -0400

    InterestPoints updated
    - Added description, title and images
    - Can now click on them properly

--- Assets/Scripts/InterestPoint.cs
+++ Assets/Scripts/InterestPoint.cs
@@ -1,13 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class InterestPoint : MonoBehaviour {
 
     public string _name;
+    public string _titleKey;
+    public string _descriptionKey;
+    public string _imagePath;
 	
-	void Update ()
+	private void Update ()
     {
         transform.LookAt(Camera.main.transform.position, Vector3.up);
+    }
+
+    public void OpenPopUp()
+    {
+        UIManager.Instance.OpenHotelViewPopUp(this);
     }
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -7,14 +7,21 @@ public class UIManager : MonoBehaviourSingleton<UIManager>
 {
     public enum Screen { HotelView, EspaceView, MapView};
 
+    [Header("Menu")]
     public Text _title;
-    public GameObject _hotelView;
-    public GameObject _espaceView;
-    public GameObject _mapView;
     public GameObject _hotelModel;
     public SpriteChanger _hotelSprite;
     public SpriteChanger _espaceSprite;
     public SpriteChanger _mapSprite;
+    [Header("Views")]
+    public GameObject _hotelView;
+    public GameObject _espaceView;
+    public GameObject _mapView;
+    [Header("Hotel View")]
+    public GameObject _hotelViewPopUp;
+    public Image _hotelViewImage;
+    public Text _hotelViewTitle;
+    public Text _hotelViewDescription;
 
     public void ChangeScreen(int screenNumber)
     {
@@ -66,5 +73,26 @@ public class UIManager : MonoBehaviourSingleton<UIManager>
             default:
                 break;
         }
+    }
+
+    public void Update()
+    {
+        // Cast a ray to check if we the user clicked on an InterestPoint
+        if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit raycastHit;
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out raycastHit, Mathf.Infinity, LayerMask.GetMask("InterestPoints")))
+            {
+                OpenHotelViewPopUp(raycastHit.collider.GetComponent<InterestPoint>());
+            }
+        }
+    }
+
+    public void OpenHotelViewPopUp(InterestPoint interestPoint)
+    {
+        _hotelViewPopUp.SetActive(true);
+        _hotelViewImage.sprite = Resources.Load<Sprite>("Images/" + interestPoint._imagePath);
+        _hotelViewTitle.text = LanguageManager.Instance.GetText(interestPoint._titleKey);
+        _hotelViewDescription.text = LanguageManager.Instance.GetText(interestPoint._descriptionKey);
     }
 }
