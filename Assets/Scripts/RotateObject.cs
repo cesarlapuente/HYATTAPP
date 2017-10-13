ac2e89cd3769f158ebdc43263c198819cf2027ad
Mythique <sv@44screens.com>
2017-10-13 10:24:28 -0400

    Added new 3D model and changed rotation speed

--- Assets/Scripts/RotateObject.cs
+++ Assets/Scripts/RotateObject.cs
@@ -3,17 +3,18 @@ using UnityEngine.EventSystems;
 
 public class RotateObject : MonoBehaviour, IDragHandler
 {
-    public float _speed;
+    public float _speedOnTablet;
+    public float _speedInEditor;
 
     public void OnDrag(PointerEventData eventData)
     {
         if (Input.touchSupported)
         {
-            transform.Rotate(new Vector3(0, -Input.touches[0].deltaPosition.x, 0) * Time.deltaTime * _speed * 0.5f);
+            transform.Rotate(new Vector3(0, -Input.touches[0].deltaPosition.x, 0) * Time.deltaTime * _speedOnTablet);
         }
         else
         {
-            transform.Rotate(new Vector3(0, -Input.GetAxis("Mouse X"), 0) * Time.deltaTime * _speed);
+            transform.Rotate(new Vector3(0, -Input.GetAxis("Mouse X"), 0) * Time.deltaTime * _speedInEditor);
         }
     }
 }
